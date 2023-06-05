893a4be3f4c18744946f73ab4486a22faf982317
MatiasBaezaGraf <matiasbaezagraf@gmail.com>
2023-06-05 10:25:26 -0300

    TEST - Get IP

--- pages/api/ip.js
+++ pages/api/ip.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
-	const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+	const ip = req.socket.remoteAddress;
 	res.status(200).json({ ip });
 }
